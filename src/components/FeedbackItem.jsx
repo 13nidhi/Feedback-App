592835ed1d34890e4e24b4691dd55eb661488bb5
Nidhi Patwa <nidhipatwa1996@gmail.com>
2022-03-28 01:55:06 +0530

    feedback first part

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,15 +1,26 @@
-import { useState } from "react";
+import {FaTimes} from 'react-icons/fa';
+import PropTypes from 'prop-types';
+import Card from './shared/Card';
 
-const FeedbackItem = ({item}) => {
+const FeedbackItem = ({item, handleDelete}) => {
 
     return (
-        <div className="card">
+        <Card reverse={true}>
             <div className="num-display">{item.rating}</div>
+            <button onClick={() => handleDelete(item.id)} className="close">
+                <FaTimes color='purple'/>
+            </button>
             <div className="text-display">
                 {item.text}
             </div>
-        </div>
+        </Card>
     )
 }
+
+FeedbackItem.prototype = {
+    item: PropTypes.object.isRequired
+}
+
+
 
 export default FeedbackItem
